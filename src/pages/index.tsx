7647f2568bbc24e9443c103bc838b0b86d04ad03
Trần Nguyễn Quy <kekkei2002@gmail.com>
2023-06-01 18:14:40 +0700

    setup mockup clients

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,78 @@
-import { Button, TextInput } from '@mantine/core';
-import clsx from 'clsx';
+import ssrAxiosClient from "@/services/backend/axiosMockups/ssrAxiosMockupClient";
+import { Product, Tag } from "@/types/Product";
+import { GetStaticPropsContext, InferGetStaticPropsType, NextPage } from "next";
+import Image from "next/image";
 
-export default function Home() {
-	return (
-		<div>
-			{/* In order for button to display properly, pls set backgroundColor. Thank you Tailwind... */}
-			<Button className='bg-primary'>Primary</Button>
-			<Button className='bg-secondary' color='customSecondary'>
-				Secondary
-			</Button>
-			<Button className='gradient'>Gradient</Button>
-			<TextInput
-				label='Mantine is awesome'
-				placeholder='Input'
-				error='Meomeo'
-				classNames={{
-					input: 'bg-primary',
-					label: 'color-primary text-xl',
-					error: clsx('text-red-500 hover:text-2xl', true && 'text-3xl'),
-				}}
-			/>
-		</div>
-	);
-}
+const HomePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  products,
+  hotTags,
+}) => {
+  return (
+    <div className="px-4 md:px-20">
+      <div className="hidden md:block">topnav</div>
+      <div>
+        <div className="flex gap-6 md:gap-7 flex-col md:flex-row">
+          <div className="bg-white h-[31.25rem] flex-1 p-5 ">
+            <div className="font-semibold text-xl">
+              <div>Hot tags</div>
+              <div className="overflow-auto h-full">
+                <div className="hidden md:block md:h-[30rem]">
+                  {hotTags.map((hotTag) => (
+                    <div key={hotTag.id} className="h-40">
+                      {hotTag.name}
+                    </div>
+                  ))}
+                </div>
+                <div className="flex md:hidden gap-3">
+                  {hotTags.map((hotTag) => (
+                    <Image
+                      className="object-cover rounded-2xl"
+                      src={"/assets/karoo.jpg"}
+                      alt="carue"
+                      width={100}
+                      height={40}
+                      draggable={false}
+                      key={hotTag.id}
+                    />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-6 md:mt-[4.875rem]">
+        {/* <FilterButtonGroup
+          onFilterChange={onFilterChange}
+          filterList={filterDashboardList}
+        />
+        <div className="flex flex-wrap gap-8 gap-x-3.75 justify-center mt-5 md:mt-11 ">
+          {products &&
+            products.map((product: any, index: number) => (
+              <BuyerProductCard key={index} {...product} />
+            ))}
+        </div> */}
+      </div>
+    </div>
+  );
+};
+
+export const getStaticProps = async (context: GetStaticPropsContext) => {
+  let products: Product[] = [];
+  let hotTags: Tag[] = [];
+  console.log("ehe");
+  try {
+    const [{ data: productData }, { data: hotTagsData }] = await Promise.all([
+      ssrAxiosClient.get<Product[]>("/products"),
+      ssrAxiosClient.get<Tag[]>("/dashboardHotTags"),
+    ]);
+    products = productData;
+    hotTags = hotTagsData;
+  } catch (e) {
+    console.error(e);
+  }
+
+  return { props: { products, hotTags }, revalidate: 10 };
+};
+
+export default HomePage;
---
+++ src/types/Product.ts
@@ -0,0 +1,17 @@
+export type Tag = {
+  id: string;
+  name: string;
+};
+
+export type Product = {
+  name: string;
+  shop: string;
+  price: string;
+  images: string[];
+};
+
+export type DashboardProduct = {
+  id: string;
+  shop: string;
+  price: string;
+};
