0a2ad7a6e8f43e4cebc1f4674edd338e448e0a74
KhanhNG <khanhngn.dev@gmail.com>
2023-06-02 16:11:22 +0700

    remove middleware

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server';
-
-export default async function middleware(request: NextRequest, response: NextResponse) {
-	// const cookies = request.cookies;
-	// await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/auth/refresh`, {
-	//   method: "POST",
-	// })
-	return NextResponse.next();
-}
