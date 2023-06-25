553e0a973039555f51d11fb03a29b0c89f12abe6
ZERO-94 <70008423+ZERO-94@users.noreply.github.com>
2023-06-25 19:37:06 +0700

    update

--- src/components/ProductDetail/ShopCard/ShopCard.tsx
+++ src/components/ProductDetail/ShopCard/ShopCard.tsx
@@ -1,29 +1,29 @@
-import { OwnerInfo } from '@/types/Product';
-import { FC, HTMLAttributes } from 'react';
-import { Rating } from '@mantine/core';
-import clsx from 'clsx';
-import Badge from '@/components/Badge/Badge';
+import { OwnerInfo } from "@/types/Product";
+import { FC, HTMLAttributes } from "react";
+import { Rating } from "@mantine/core";
+import clsx from "clsx";
+import Badge from "@/components/Badge/Badge";
 
 type ArtistInfoProps = HTMLAttributes<HTMLDivElement> & {
-	artist: OwnerInfo;
+  artist: OwnerInfo;
 };
 
 const ShopCard: FC<ArtistInfoProps> = ({ artist, className, ...rest }) => {
-	return (
-		<div
-			className={clsx('rounded-lg md:rounded-full p-5 bg-white flex h-max', className)}
-			{...rest}
-		>
-			<div className='w-20 gradient aspect-square rounded-full'></div>
-			<div className='flex flex-col ml-5'>
-				<h3 className='text-2xl font-bold'>{artist.displayName}</h3>
-				{/* <Rating value={artist.rating} color='customPrimary' readOnly /> */}
-				<Badge className='mt-auto' size='text-md'>
-					BLAH BLAH
-				</Badge>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={clsx(
+        "rounded-lg md:rounded-full p-5 bg-white flex h-max",
+        className
+      )}
+      {...rest}
+    >
+      <div className="w-20 gradient aspect-square rounded-full"></div>
+      <div className="flex flex-col ml-5">
+        <h3 className="text-2xl font-bold">{artist.displayName}</h3>
+        <span className="text-gray-500 text-xl">@{artist.username}</span>
+      </div>
+    </div>
+  );
 };
 
 export default ShopCard;
