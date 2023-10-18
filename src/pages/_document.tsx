e8716d16c3383afe7455f811de43de1c684fe30a
AnhTVK <70008423+ZERO-94@users.noreply.github.com>
2023-10-18 20:48:29 +0700

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,17 +1,16 @@
-import { createGetInitialProps } from '@mantine/next';
-import { Head, Html, Main, NextScript } from 'next/document';
+import { createGetInitialProps } from "@mantine/next";
+import { Head, Html, Main, NextScript } from "next/document";
 
 // For Mantine
 export const getInitialProps = createGetInitialProps();
 
 export default function Document() {
-	return (
-		<Html lang='en'>
-			<Head />
-			<body>
-				<Main />
-				<NextScript />
-			</body>
-		</Html>
-	);
+  return (
+    <Html lang="en">
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
