7c9a9d352bdd2ea4e99bd4dbaace6b93bad6461b
trongnhandev1011 <82007391+trongnhandev1011@users.noreply.github.com>
2023-10-19 20:45:49 +0700

    restyling homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,6 +17,8 @@ import { useRouter } from "next/router";
 import { ROUTE } from "@/constants/route";
 import Image from "next/image";
 import ImageWithFallback from "@/components/ImageWithFallback/ImageWithFallback";
+import { Shop } from "@/types/Shop";
+import ShopPreviewCard from "@/components/ShopPreviewCard/ShopPreviewCard";
 
 const TAG_COLORS = [
   "#FF9898",
@@ -33,14 +35,16 @@ const HomePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   hotTags,
   homeBranding,
   categories,
+  shops,
 }) => {
-  const { data: products, isLoading } = useSWR(["/product"], (key) =>
+  const { data: products, isLoading } = useSWR(["/product?pageSize=6"], (key) =>
     axiosClient
       .get<CommonResponseBase<{ items: Product[] }>>(key[0])
       .then((res) => res.data.data)
   );
 
   const router = useRouter();
+  console.log(shops);
 
   return (
     <div className="home-page flex flex-col gap-8 lg:gap-16">
@@ -66,37 +70,6 @@ const HomePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
             >
               <div className="font-bold">{tag.name}</div>
             </div>
-          )}
-        />
-      </div>
-      <div>
-        <div className="flex justify-between">
-          <div className="flex text-center font-semibold">
-            <div className="w-[7rem] sm:w-[12.5rem] bg-white rounded-xl p-1">
-              Hot
-            </div>
-            <div className="w-[7rem] sm:w-[12.5rem] text-[#AFAFAF] p-1">
-              For me
-            </div>
-          </div>
-          <div
-            className="font-semibold hidden lg:block cursor-pointer"
-            onClick={() => router.push(ROUTE.PRODUCT_LIST)}
-          >
-            Xem tất cả
-          </div>
-        </div>
-        <PreviewList
-          data={hotProducts}
-          classNames={{
-            root: "mt-5",
-            container: "gap-x-4 xl:gap-x-6",
-            slide:
-              "w-[calc((100vw-(1.0895rem*2)-(1.5rem*2))/2)] sm:w-[calc((100vw-(1.0585rem*3)-(1.5rem*2))/3)] md:w-[calc((100vw-(1.0435rem*4)-(1.5rem*2))/4)] flex-none lg:flex-1",
-            controls: "hidden lg:flex",
-          }}
-          render={(product: Product) => (
-            <ProductPreviewCard data={product} key={product.name} />
           )}
         />
       </div>
@@ -126,15 +99,60 @@ const HomePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
         </div>
       </div>
       <div>
-        <div className="font-semibold">Gợi ý cho bạn hôm nay</div>
+        <div className="flex justify-end">
+          {/* <div className="flex text-center font-semibold">
+            <div className="w-[7rem] sm:w-[12.5rem] bg-white rounded-xl p-1">
+              Hot
+            </div>
+            <div className="w-[7rem] sm:w-[12.5rem] text-[#AFAFAF] p-1">
+              For me
+            </div>
+          </div> */}
+          <div
+            className="font-semibold hidden lg:block cursor-pointer"
+            onClick={() => router.push(ROUTE.PRODUCT_LIST)}
+          >
+            Xem tất cả
+          </div>
+        </div>
+        <div>
+          <div className="font-semibold">Gợi ý cho bạn hôm nay</div>
+          <div
+            className={clsx(
+              productStyles["product-list-grid"],
+              "mt-3 lg:!grid-cols-6"
+            )}
+          >
+            {products?.items.map((product, index) => (
+              <ProductPreviewCard data={product} key={index} />
+            ))}
+          </div>
+        </div>
+        {/* <PreviewList
+          data={hotProducts}
+          classNames={{
+            root: "mt-5",
+            container: "gap-x-4 xl:gap-x-6",
+            slide:
+              "w-[calc((100vw-(1.0895rem*2)-(1.5rem*2))/2)] sm:w-[calc((100vw-(1.0585rem*3)-(1.5rem*2))/3)] md:w-[calc((100vw-(1.0435rem*4)-(1.5rem*2))/4)] flex-none lg:flex-1",
+            controls: "hidden lg:flex",
+          }}
+          render={(product: Product) => (
+            <ProductPreviewCard data={product} key={product.name} />
+          )}
+        /> */}
+        <div className="flex justify-between mt-20 items-center">
+          <div className="font-bold text-xl">Shop nhà Arty</div>
+          <div>Xem tất cả</div>
+        </div>
         <div
           className={clsx(
             productStyles["product-list-grid"],
-            "mt-3 lg:!grid-cols-5"
+            "mt-3 lg:!grid-cols-6"
           )}
         >
-          {products?.items.map((product, index) => (
-            <ProductPreviewCard data={product} key={index} />
+          {shops?.map((shop, index) => (
+            <ShopPreviewCard shop={shop} key={index} />
           ))}
         </div>
       </div>
@@ -147,23 +165,31 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
   let hotTags: Tag[] = [];
   let homeBranding: HomeBranding | null = null;
   let categories: Category[] = [];
+  let shops: Shop[] = [];
   try {
-    const [{ data: productRes }, { data: hotTagsRes }, { data: categoryRes }] =
-      await Promise.all([
-        axiosClient.get<CommonResponseBase<PaginationResponseBase<Product>>>(
-          "/product?pageSize=5"
-        ),
-        axiosClient.get<CommonResponseBase<PaginationResponseBase<Tag>>>(
-          "/tag?pageSize=12"
-        ),
-
-        axiosClient.get<CommonResponseBase<PaginationResponseBase<Category>>>(
-          "/category?pageSize=8"
-        ),
-      ]);
+    const [
+      { data: productRes },
+      { data: hotTagsRes },
+      { data: categoryRes },
+      { data: shopRes },
+    ] = await Promise.all([
+      axiosClient.get<CommonResponseBase<PaginationResponseBase<Product>>>(
+        "/product?pageSize=4"
+      ),
+      axiosClient.get<CommonResponseBase<PaginationResponseBase<Tag>>>(
+        "/tag?pageSize=12"
+      ),
+      axiosClient.get<CommonResponseBase<PaginationResponseBase<Category>>>(
+        "/category?pageSize=8"
+      ),
+      axiosClient.get<CommonResponseBase<PaginationResponseBase<Shop>>>(
+        "/shop?pageSize=6"
+      ),
+    ]);
     hotProducts = productRes.data.items;
     hotTags = hotTagsRes.data.items;
     categories = categoryRes.data.items;
+    shops = shopRes.data.items;
   } catch (e) {
     console.error(e);
   }
@@ -179,6 +205,7 @@ export const getStaticProps = async (context: GetStaticPropsContext) => {
       }),
       homeBranding,
       categories,
+      shops,
     },
     revalidate: 10,
   };
