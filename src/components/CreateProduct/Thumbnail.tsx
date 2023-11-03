f01f48e1a8acb9d811ec9c1245751274066fd4f9
AnhTVK <70008423+ZERO-94@users.noreply.github.com>
2023-11-03 23:10:22 +0700

    Update Thumbnail.tsx

--- src/components/CreateProduct/Thumbnail.tsx
+++ src/components/CreateProduct/Thumbnail.tsx
@@ -13,6 +13,7 @@ type ThumbnailProps = HTMLAttributes<HTMLDivElement> & {
   defaultPlaceholder?: React.ReactNode;
   clearable?: boolean;
   onClear?: () => void;
+  disabled?: boolean;
 };
 
 const Thumbnail: FC<ThumbnailProps> = ({
@@ -26,6 +27,7 @@ const Thumbnail: FC<ThumbnailProps> = ({
   defaultPlaceholder = <div>+</div>,
   clearable = false,
   onClear,
+  disabled = false,
   ...rest
 }) => {
   const [imageUrl, setImageUrl] = useState(url);
@@ -70,12 +72,16 @@ const Thumbnail: FC<ThumbnailProps> = ({
       <input
         type="file"
         name="thumbnail"
-        className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer peer"
+        className={clsx(
+          "absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer peer",
+          disabled && "pointer-events-none"
+        )}
         onChange={uploadHandler}
         accept="image/*"
         value=""
+        readOnly={disabled}
       />
-      {imageUrl && (
+      {!disabled && imageUrl && (
         <div className="absolute top-0 left-0 w-full h-full pointer-events-none peer-hover:opacity-100 hover:opacity-100 opacity-0 bg-black/50 transition-opacity flex flex-col items-center justify-center gap-1">
           <div className="text-white text-center">Update</div>
           {clearable && (
