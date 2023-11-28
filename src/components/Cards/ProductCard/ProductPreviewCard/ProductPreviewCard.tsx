44ea0382e136f302216fd4cb01562ce16580242e
AnhTVK <70008423+ZERO-94@users.noreply.github.com>
2023-11-28 19:53:28 +0700

    udate

--- src/components/Cards/ProductCard/ProductPreviewCard/ProductPreviewCard.tsx
+++ src/components/Cards/ProductCard/ProductPreviewCard/ProductPreviewCard.tsx
@@ -5,6 +5,8 @@ import clsx from "clsx";
 import { useRouter } from "next/router";
 import defaultImg from "../../../../../public/assets/default-thumbnail.jpg";
 import styles from "./ProductPreviewCard.module.scss";
+import { getCampaignType } from "@/utils/mapper";
+import { CAMPAIGN_TYPE_DATA } from "@/constants/campaign";
 
 interface IProductPreviewCardProps {
   data: Product;
@@ -13,17 +15,37 @@ interface IProductPreviewCardProps {
 
 const ProductPreviewCard = ({ data, className }: IProductPreviewCardProps) => {
   const router = useRouter();
+
+  const campaignType = getCampaignType(data.saleCampaign);
+
+  const campaignTypeData = CAMPAIGN_TYPE_DATA[campaignType];
+
   return (
     <div
       className={clsx(
         styles["product-preview-card"],
-        "bg-white rounded-2xl !aspect-[3/5] w-full cursor-pointer shadow",
+        "bg-white rounded-2xl !aspect-[3/5] w-full cursor-pointer shadow relative",
         className
       )}
       onClick={() =>
         router.push(`/product/${data.saleCampaign.id}_${data.productCode}`)
       }
     >
+      {data.saleCampaign.type !== "SHARE" && (
+        <div
+          className={clsx(
+            "absolute right-0 top-[5%] z-10 pl-2 pr-2 py-1 rounded-l-lg font-semibold text-sm",
+            campaignTypeData.textStyle,
+            "bg-primary"
+          )}
+        >
+          {data.saleCampaign.type === "PRIVATE"
+            ? `Shop Only`
+            : data.saleCampaign.type === "PUBLIC"
+            ? "Arty Only"
+            : null}
+        </div>
+      )}
       <div className="relative w-full aspect-square">
         <ImageWithFallback
           fallback="/assets/default-thumbnail.jpg"
